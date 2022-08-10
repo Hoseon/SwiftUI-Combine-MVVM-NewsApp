ffcb6f59082f99916bb3c482b5fdd2b0db23ed62
HoseonChu <chs1025@gmail.com>
2022-08-11 08:06:41 +0900

    Initial Commit

---
+++ TestNewsApp/TestNewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestNewsAppApp.swift
+//  TestNewsApp
+//
+//  Created by HoSeon Chu on 2022/08/11.
+//
+
+import SwiftUI
+
+@main
+struct TestNewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
